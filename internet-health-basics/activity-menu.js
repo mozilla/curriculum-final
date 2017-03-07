9c68530e7068b33aa692011a8922982f98f87850
chadsansing <csansing@gmail.com>
2017-03-07 11:10:52 -0500

    Added Internet Health Basics project

---
+++ internet-health-basics/activity-menu.js
@@ -0,0 +1,28 @@
+var title = "Internet Health Basics";
+
+var activities = [
+{
+  title : "An Introduction to Internet Health",
+  url: "session01-intro-to-internet-health.html"
+},
+{
+  title : "Web Literacy Activity",
+  url: "session02-web-literacy.html"
+},
+{
+title : "Digital Inclusion Activity",
+url: "session03-digital-inclusion.html"
+},
+{
+title : "Privacy and Security Activity",
+url: "session04-privacy-and-security.html"
+},
+{
+title : "Decentralization Activity",
+url: "session05-decentralization.html"
+},
+{
+title : "Open Innovation Activity",
+url: "session06-open-innovation.html"
+}
+];
