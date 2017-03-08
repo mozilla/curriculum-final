627c7f110cd0861553c83f9725a319c30e2ab0c3
chadsansing <csansing@gmail.com>
2017-03-08 09:31:42 -0500

    Fixed nav numbering & updated up-next links

--- intermediate-web-lit-one/activity-menu.js
+++ intermediate-web-lit-one/activity-menu.js
@@ -11,26 +11,26 @@ var activities = [
   },
   {
     title: "CSS Fonts Extension Activity",
-    url: "session02a-css-fonts-extension-activity.html"
+    url: "session03-css-fonts-extension-activity.html"
   },
   {
     title : "CSS Circles",
-    url: "session03-css-circles.html"
+    url: "session04-css-circles.html"
   },
   {
     title : "CSS Blocks",
-    url: "session04-css-building-blocks.html"
+    url: "session05-css-building-blocks.html"
   },
   {
     title : "CSS Decoration",
-    url: "session05-css-decoration.html"
+    url: "session06-css-decoration.html"
   },
   {
     title : "Webotron &amp; the Scrambled Divs",
-    url: "session06-webotron-and-the-scrambled-divs.html"
+    url: "session07-webotron-and-the-scrambled-divs.html"
   },
   {
     title : "External CSS Tutorials",
-    url: "session07-external-css-tutorials.html"
+    url: "session08-external-css-tutorials.html"
   }
 ];
