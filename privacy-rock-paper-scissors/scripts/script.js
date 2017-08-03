fb4dc88e8bda84f5f77fd89146eafae7078b811c
mozilla <luke.pacholski@gmail.com>
2017-08-03 15:34:40 -0700

    Updating rock paper scissors game

--- privacy-rock-paper-scissors/scripts/script.js
+++ privacy-rock-paper-scissors/scripts/script.js
@@ -1,68 +1,114 @@
+var playerLife = 10;
+var hackerLife = 10;
+
+
+var hackerCards = [
+  {
+    description : "I looked over your shoulder while you typed your password.",
+    power : 4,
+  },
+  {
+    description : "I looked over your shoulder while you typed your password.",
+    power : 2,
+  }
+]
+
+var playerCards = [
+  {
+    description : "I always use the same password.",
+    power : 4,
+  },
+  {
+    description : "I write my password down on a post-it",
+    power : 2,
+  }
+]
+
+function gameOver(winner){
+  console.log("Winner is: " + winner);
+  document.querySelector(".game-board").classList.add("game-over");
+  document.querySelector(".winner-message").innerHHTML = winner + " wins!";
+}
+
+
 function playGame() {
-  
-  //myPlay function
-  
-  function myPlay(description, points) {
-    
-    this.description = description;
-    this.points = points;
-    
-  }
-  
-  //opponentPlay function
-  
-  function opponentPlay(description, points) {
-    
-    this.description = description;
-    this.points = points;
-    
-  }
-  
-  // myPlay objects
-  
-  var samePassword = new myPlay("I always use the same password.", -1);
-  var differentPassword = new myPlay("I always use different passwords.", 0);
-  var passwordManager = new myPlay("I use a password manager.", 1);
-  
-  // opponentPlay objects
-  
-  var gotThemAll = new opponentPlay("I cracked your password and got into all your accounts!", -1);
-  var gotOne = new opponentPlay("I cracked a password and got into one account!", 0);
-  var gotNothing = new opponentPlay("I got nothing.", 1);
-  
-  // Variables for playing Privacy Rock-Paper-Scissors
-  
-  var myPlays  = [samePassword, differentPassword, passwordManager];
-  var opponentPlays = [gotThemAll, gotOne, gotNothing];
-  // var score = 0;
-  // this.score = score;
-  
-  //Maths for playing Privacy Rock-Paper-Scissors
-  
-  var outcome = Math.round(Math.random()*3);
-  this.outcome = outcome;
 
-  var myOutcome = myPlays[this.outcome-1];
-  var opponentOutcome = opponentPlays[this.outcome-1];
-  
-  this.myOutcome = myOutcome;
-  this.opponentOutcome = opponentOutcome;
-  
-  if(this.outcome < 2){
-    document.getElementById("results").innerHTML = "<div>You lose!</div>";
-  }
-  else if(this.outcome > 2) {
-    document.getElementById("results").innerHTML = "<div>You win!</div>";
-  }
-  else {
-    document.getElementById("results").innerHTML = "<div>It's a draw!</div>";
+  //Show cards
+  var cards = document.querySelectorAll(".card");
+
+  document.querySelector("button").setAttribute("disabled", "true");
+
+
+  for(var i = 0; i < cards.length; i++) {
+    cards[i].style.display = "none";
+    cards[i].classList.remove("worse-card");
+    cards[i].classList.remove("better-card");
+    cards[i].classList.remove("tie-card");
   }
 
-  
-  // What shows up on the webpage after each round
-  
-  document.getElementById("opponent-play-space").innerHTML = "<div>" + this.opponentOutcome.description + "</div>";
-  document.getElementById("my-play-space").innerHTML = "<div>" + this.myOutcome.description + "</div>";
-  // document.getElementById("score").innerHTML ="<div>" + this.score + "</div>";
-  
+  // Pick the player card
+  var randomPlayerCardIndex = Math.floor(Math.random() * (playerCards.length));
+  var playerCard = playerCards[randomPlayerCardIndex];
+  document.querySelector("#player-card .text").innerHTML = playerCard.description;
+  document.querySelector("#player-card .power").innerHTML = playerCard.power;
+
+
+  // Picks the hacker card
+  var randomhackerCardIndex = Math.floor(Math.random() * (hackerCards.length));
+  var hackerCard = hackerCards[randomhackerCardIndex];
+  document.querySelector("#hacker-card .text").innerHTML = hackerCard.description;
+  document.querySelector("#hacker-card .power").innerHTML = hackerCard.power;
+
+  setTimeout(function(){
+    for(var i = 0; i < cards.length; i++) {
+      cards[i].style.display = "block";
+    }
+  },200)
+
+
+  setTimeout(function(){
+    var powerDifference = playerCard.power - hackerCard.power;
+
+    if(powerDifference < 0) {
+      playerLife = playerLife + powerDifference;
+      document.querySelector("#hacker-card").classList.add("better-card");
+      document.querySelector("#player-card").classList.add("worse-card");
+    } else if (powerDifference > 0) {
+      hackerLife = hackerLife - powerDifference;
+      document.querySelector("#player-card").classList.add("better-card");
+      document.querySelector("#hacker-card").classList.add("worse-card");
+    } else {
+      document.querySelector("#player-card").classList.add("tie-card");
+      document.querySelector("#hacker-card").classList.add("tie-card");
+    }
+
+    // Update life totals
+    document.querySelector(".player-stats .life-total").innerHTML = playerLife;
+    document.querySelector(".hacker-stats .life-total").innerHTML = hackerLife;
+
+
+    if(playerLife <= 0) {
+      gameOver("Hacker");
+    } else if (hackerLife <= 0){
+      gameOver("Player")
+    }
+
+      document.querySelector("button").removeAttribute("disabled");
+
+  },1000)
+
+
+
+
+
+
+
+
+}
+
+function opponentPlay(description, points) {
+
+  this.description = description;
+  this.points = points;
+
 }
