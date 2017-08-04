a852f03645d8c3a368ef064cbc2333cbe24a01e2
mozilla <luke.pacholski@gmail.com>
2017-08-04 10:33:07 -0700

    Moved cards to the cards.js file

---
+++ privacy-rock-paper-scissors/scripts/cards.js
@@ -0,0 +1,22 @@
+
+var hackerCards = [
+  {
+    description : "I looked over your shoulder while you typed your password.",
+    power : 5,
+  },
+  {
+    description : "I looked over your shoulder while you typed your password.",
+    power : 1,
+  }
+]
+
+var playerCards = [
+  {
+    description : "I always use the same password.",
+    power : 2,
+  },
+  {
+    description : "I write my password down on a post-it",
+    power : 2,
+  }
+]
--- privacy-rock-paper-scissors/scripts/script.js
+++ privacy-rock-paper-scissors/scripts/script.js
@@ -6,31 +6,8 @@ var hackerLife = 5;
 var playerStartLife = parseInt(playerLife);
 var hackerStartLife = parseInt(hackerLife);
 
-
-
 updateScores();
 
-var hackerCards = [
-  {
-    description : "I looked over your shoulder while you typed your password.",
-    power : 5,
-  },
-  {
-    description : "I looked over your shoulder while you typed your password.",
-    power : 1,
-  }
-]
-
-var playerCards = [
-  {
-    description : "I always use the same password.",
-    power : 2,
-  },
-  {
-    description : "I write my password down on a post-it",
-    power : 2,
-  }
-]
 
 function gameOver(winner) {
   document.querySelector(".game-board").classList.add("game-over");
