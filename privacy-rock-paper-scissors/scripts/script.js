2d924c6fe9354135038f25978921ce3551a5d7ff
mozilla <luke.pacholski@gmail.com>
2017-08-04 10:09:53 -0700

    Added emoji avatars and winner screen

--- privacy-rock-paper-scissors/scripts/script.js
+++ privacy-rock-paper-scissors/scripts/script.js
@@ -1,23 +1,30 @@
-var startingLife = 10
-var playerLife = startingLife;
-var hackerLife = startingLife;
+// Set starting life totals here
+var playerLife = 5;
+var hackerLife = 5;
 
+
+var playerStartLife = parseInt(playerLife);
+var hackerStartLife = parseInt(hackerLife);
+
+
+
+updateScores();
 
 var hackerCards = [
   {
     description : "I looked over your shoulder while you typed your password.",
-    power : 4,
+    power : 5,
   },
   {
     description : "I looked over your shoulder while you typed your password.",
-    power : 2,
+    power : 1,
   }
 ]
 
 var playerCards = [
   {
     description : "I always use the same password.",
-    power : 4,
+    power : 2,
   },
   {
     description : "I write my password down on a post-it",
@@ -25,14 +32,44 @@ var playerCards = [
   }
 ]
 
-function gameOver(winner){
-  console.log("Winner is: " + winner);
+function gameOver(winner) {
   document.querySelector(".game-board").classList.add("game-over");
-  document.querySelector(".winner-message").innerHHTML = winner + " wins!";
+  document.querySelector(".winner-message").innerHTML = winner + " wins!";
+  document.querySelector(".winner-section").style.display = "block";
+}
+
+function restartGame(){
+  document.querySelector(".game-board").classList.remove("game-over");
+  document.querySelector(".winner-section").style.display = "none";
+  document.querySelector("#hacker-card").style.display = "none";
+  document.querySelector("#player-card").style.display = "none";
+
+
+  playerLife = playerStartLife;
+  hackerLIfe = hackerStartLife;
 }
 
 
+function updateScores(){
+  // Update life totals
+  document.querySelector(".player-stats .life-total").innerHTML = playerLife;
+  document.querySelector(".hacker-stats .life-total").innerHTML = hackerLife;
 
+  // Set player lifebar
+  var playerPercent = playerLife / playerStartLife * 100;
+  if (playerPercent < 0) {
+    playerPercent = 0;
+  }
+  document.querySelector(".player-stats .life-left").style.height =  playerPercent + "%";
+
+  var hackerPercent = hackerLife / hackerStartLife * 100
+  if (hackerPercent < 0) {
+    hackerPercent = 0;
+  }
+  document.querySelector(".hacker-stats .life-left").style.height =  hackerPercent + "%";
+
+
+}
 
 function playGame() {
 
@@ -40,7 +77,6 @@ function playGame() {
   var cards = document.querySelectorAll(".card");
 
   document.querySelector("button").setAttribute("disabled", "true");
-
 
   for(var i = 0; i < cards.length; i++) {
     cards[i].style.display = "none";
@@ -55,6 +91,9 @@ function playGame() {
   document.querySelector("#player-card .text").innerHTML = playerCard.description;
   document.querySelector("#player-card .power").innerHTML = playerCard.power;
 
+  // Remove the "ouch" class animation
+  document.querySelector(".hacker-stats .thumbnail").classList.remove("ouch");
+  document.querySelector(".player-stats .thumbnail").classList.remove("ouch");
 
   // Picks the hacker card
   var randomhackerCardIndex = Math.floor(Math.random() * (hackerCards.length));
@@ -76,22 +115,18 @@ function playGame() {
       playerLife = playerLife + powerDifference;
       document.querySelector("#hacker-card").classList.add("better-card");
       document.querySelector("#player-card").classList.add("worse-card");
+      document.querySelector(".player-stats .thumbnail").classList.add("ouch");
     } else if (powerDifference > 0) {
       hackerLife = hackerLife - powerDifference;
       document.querySelector("#player-card").classList.add("better-card");
       document.querySelector("#hacker-card").classList.add("worse-card");
+      document.querySelector(".hacker-stats .thumbnail").classList.add("ouch");
     } else {
       document.querySelector("#player-card").classList.add("tie-card");
       document.querySelector("#hacker-card").classList.add("tie-card");
     }
 
-    // Update life totals
-    document.querySelector(".player-stats .life-total").innerHTML = playerLife;
-    document.querySelector(".hacker-stats .life-total").innerHTML = hackerLife;
-
-    document.querySelector(".hacker-stats .life-left").style.height = hackerLife / startingLife * 100 + "%";
-    document.querySelector(".player-stats .life-left").style.height = playerLife / startingLife * 100 + "%";
-
+    updateScores();
 
     if(playerLife <= 0) {
       gameOver("Hacker");
