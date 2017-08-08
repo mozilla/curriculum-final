31e0cadf2bafebec9f431d78d343c2e8d05f74e4
chadsansing <csansing@gmail.com>
2017-08-08 13:59:55 -0400

    Added new player and hacker cards

--- privacy-rock-paper-scissors/scripts/cards.js
+++ privacy-rock-paper-scissors/scripts/cards.js
@@ -1,12 +1,44 @@
 
 var hackerCards = [
   {
-    description : "I looked over your shoulder while you typed your password.",
+    description : "I took over all your accounts because you always use the same password.",
     power : 5,
+  },
+  {
+    description : "I took over one of your accounts, but you have different passwords for the others.",
+    power : 3,
   },
   {
     description : "I looked over your shoulder while you typed your password.",
     power : 1,
+  },
+  {
+    description: "I know where you like to spend money because of your check-ins on apps.",
+    power: 3,
+  },
+  {
+    description: "I intercepted your encrypted emails and messages, but couldn't read them.",
+    power: 1,
+  },
+  {
+    description: "I intercepted your unencrypted emails and messages.",
+    power: 4,
+  },
+  {
+    description : "I figure out who you are from the info you shared in an onine game.",
+    power : 2,
+  },
+  {
+    description : "I tracked you online and got your browsing history.",
+    power : 3, 
+  },
+  {
+    description: "I impersonated you online using information you shared about yourself on the web.",
+    power : 4,
+  },
+  {
+    description : "I tricked you into giving me your password with a phony email.",
+    power: 4,
   }
 ]
 
@@ -16,7 +48,55 @@ var playerCards = [
     power : 2,
   },
   {
-    description : "I write my password down on a post-it",
+    description : "I write my password down on a post-it.",
     power : 2,
+  },
+  {
+    description : "I use a password manager.",
+    power : 4,
+  },
+  {
+    description : "I only go to websites with https://.",
+    power : 3,
+  },
+  {
+    description: "I always use private browsing.",
+    power : 3,
+  },
+  {
+    description: "I always erase my browsing history.",
+    power : 3,
+  },
+  {
+    description: "I use a VPN when I go online.",
+    power: 4,
+  },
+  {
+    description: "I use TOR with maxed out privacy settings when I go online.",
+    power: 5,
+  },
+  {
+    description: "I turn off location services on all of my apps.",
+    power: 4,
+  },
+  {
+    description : "I always tag myself and my location on every app.",
+    power: 1,
+  },
+  {
+    description : "I tell people all about myself in the games I play.",
+    power : 1,
+  },
+  {
+    description: "Everything I do online is public.",
+    power : 1,
+  },
+  {
+    description : "I don't share personal information with strangers online.",
+    power : 3,
+  },
+  {
+    description : "I use encrypted email and messaging.",
+    power : 4,
   }
 ]
